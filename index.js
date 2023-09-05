f8633eb4d341398883573d745be50d4b013b3fe8
k-huber <141048452+k-huber@users.noreply.github.com>
2023-09-05 20:48:10 +0200

    add bmi rechner usw.

---
+++ index.js
@@ -0,0 +1,38 @@
+/*const namesWomen = ['kathrin', 'anna', 'linda'];
+const namesNew = namesWomen.slice(0, 2);
+console.log(namesNew);
+console.log(namesWomen);
+
+let test = document.querySelectorAll('');
+const numbers = [1, 4, 5, 13, 16, 23, 7, 51];
+
+function biggestNumberInArray(numberArray) {
+    let largest = null;
+    for (let number of numberArray) {
+        if (number > largest) {
+            largest = number;
+        }
+    }
+    return largest;
+}
+console.log(biggestNumberInArray(numbers));
+
+console.log(numbers.sort(function (a, b) { return a - b; }).reverse());
+
+let evenNumbers = numbers.filter(x => x / 2 === 0);
+
+console.log(evenNumbers);
+
+
+const faqSection = document.querySelectorAll('.faq-section');
+
+function toggleVisibility(domItem, childElement) {
+    const text = domItem.childElement;
+    text.classList.toggle('text-visible');
+}
+
+faqSection.forEach((item) => {
+    item.addEventListener('click', () => {
+        toggleVisibility(item, lastElementChild);
+    })
+});*/
